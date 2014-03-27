3bc2b047384dc3fea067a7e0d35bd19ca10549ef
andrey59 <andrey@kskmedia.ru>
2014-03-27 15:09:33 +0600

    изменил logo

--- js/bootstrap-tabdrop.js
+++ js/bootstrap-tabdrop.js
@@ -17,7 +17,7 @@
  * limitations under the License.
  * ========================================================= */
  
-+function( $ ) {
+!function( $ ) {
 
 	var WinReszier = (function(){
 		var registered = [];
