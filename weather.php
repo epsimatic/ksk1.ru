7cb74c8cc5b3a73fe229ae096b43ca363ac1a661
andrey59 <andrey@kskmedia.ru>
2014-10-14 18:07:34 +0600

    создал файл  weather.php

---
+++ weather.php
@@ -0,0 +1,50 @@
+<?php
+$json_string = file_get_contents("http://api.wunderground.com/api/14a26adef7c89cc2/geolookup/conditions/lang:RU/q/Russia/Krasnoufimsk.json");
+$parsed_json = json_decode($json_string);
+$location = $parsed_json->{'location'}->{'city'};
+$temp_c = intval($parsed_json->{'current_observation'}->{'temp_c'});
+$pressure = round(intval($parsed_json->{'current_observation'}->{'pressure_mb'}) * 0.7500637554192);
+$wind = round(intval($parsed_json->{'current_observation'}->{'wind_kph'}) / 3.6);
+$humidity = $parsed_json->{'current_observation'}->{'relative_humidity'};
+$feelslike_c = intval($parsed_json->{'current_observation'}->{'feelslike_c'});
+if ($temp_c == $feelslike_c) $temp = 'Температура ' . $temp_c . '°';
+else  $temp = 'Температура ' . $temp_c . '°, Ощущается как ' . $feelslike_c . '°';
+if (is_nan($temp_c)) {
+    header("Status: 503 Internal server error");
+    die ('Error saving weather.html temp_c isNAN');
+}
+$icon_url = $parsed_json->{'current_observation'}->{'icon_url'};
+if ($temp_c > 0) $sign = "+"; else $sign = "";
+$text = '<a class="weather-block header-line__weather-block" rel="nofollow" target="_blank"
+             style="opacity: 1;" title="По данным на ' .
+    date("G.i", $parsed_json->{'current_observation'}->{'observation_epoch'}) . ':' . PHP_EOL
+    . $temp . '
+Давление ' . $pressure . ' мм рт.ст.
+Ветер ' . $wind . ' м/с
+Влажность ' . $humidity . PHP_EOL
+    . $parsed_json->{'current_observation'}->{'weather'} . '
+Щёлкните для прогноза">
+            <div class="weather-icon header-line__weather-block__icon"><img src="' . $icon_url . '"></div>
+            <div class="weather-temp header-line__logo__weather-block__temp">' . $sign . $temp_c . '</div>
+            <div class="header-line__weather-block__label">' . $parsed_json->{'current_observation'}->{'weather'} . '</div>
+        </a>';
+
+if (file_put_contents("weather.html", $text)) {
+    echo "File weather.html saved";
+} else {
+    header("Status: 503 Internal server error");
+    die ('Error saving weather.html');
+}
+
+
+/*echo "<p>Current temperature in ${location} is: ${temp_c}</p>";
+echo "<p>";
+
+echo date("G:i",($parsed_json->{'current_observation'}->{'observation_epoch'}));
+
+echo "</p>";*/
+
+
+//echo "time =".date("G:i",($parsed_json->{'current_observation'}->{'local_epoch'}+21600));
+//echo date("Z");
+?>
