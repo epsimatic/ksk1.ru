e984b82d2d281961714d064d30c65d704192fdb4
andrey59 <andrey@kskmedia.ru>
2015-05-19 14:31:09 +0500

    Заменил значок градуса для text_day

--- weather/get_weather.php
+++ weather/get_weather.php
@@ -140,7 +140,8 @@ foreach ($array_forecast as $forecast_object) {
     $text_night = mbStringToArray($forecast_object['text_night']);
     $text_night[0] = mb_convert_case($text_night[0], MB_CASE_LOWER);
     $text_night = implode("", $text_night);
-    $text_day   = str_replace("C.", "℃.", $forecast_object['text_day']);
+    $text_day = $forecast_object['text_day'];
+    $text_day   = str_replace("C.", "℃.", $text_day);
     $text_night = str_replace("C.", "℃.", $text_night);
     $conditions_forecast .= " <div class='day-row'>
                         <div class='summary'>
