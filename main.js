8b3c65515195feb72e873e441df44ffcc1a6db79
andrey59 <andrey@kskmedia.ru>
2015-06-18 10:18:00 +0500

    кнопки "Спаравочник" и  "Карта" на панели в Городе активизируют открытие панели Услуги

--- main.js
+++ main.js
@@ -48,8 +48,8 @@ function setMapHeight() {
         window.setTimeout(map.invalidateSize, 800);
     }
 }
-
-
+// кнопки "Спаравочник" и  "Карта" на панели в Городе
+jQuery('.cat-feature , .map-feature').click( function() {jQuery('#btn-feature-services').click();} );
 // Кнопки НавПанели и выдвижные панельки
 jQuery('.triggers-weather').click( function() {jQuery('#btn-feature-info').click();} );
 
