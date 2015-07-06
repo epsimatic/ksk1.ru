752baf70f2211ccdeff2d17a55b7a8b4388213da
andrey59 <andrey@kskmedia.ru>
2015-07-06 17:28:54 +0500

    написал абсолютный путь для загрузки movies-block

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ jQuery('#navpanel-info').one('first-load', function () {
 
         var day_num = jQuery(this).data('day');
 //        console.log('день = '+day_num);
-        jQuery("#day"+day_num).load("/movies-block/",{"day_week":day_num}, function() {
+        jQuery("#day"+day_num).load("http://news.kskmedia.ru/movies-block/",{"day_week":day_num}, function() {
             if (day_num > 1 && day_num < 7)
                 jQuery('.list-days-ajax a[data-day="'+(day_num+1)+'"]').trigger('shown.bs.tab');
         });
