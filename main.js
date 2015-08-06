e072914bf8858fd839cf52e7f2e2beeba95e2350
andrey59 <andrey@kskmedia.ru>
2015-08-06 17:08:57 +0500

    Вставил проверку наличия переменной map

--- main.js
+++ main.js
@@ -314,7 +314,7 @@ function AddMap(name_id,map_height){
 
 jQuery('#navpanel-services').one('first-load', function () {
 // Загружаем карту
-    if(isset('map'))
+    if(!isset('map'))
     AddMap('panel-map',428);
    /* setMapHeight();
     LoadCSS('http://ksk1.ru/vendor/leaflet/dist/leaflet.css');
