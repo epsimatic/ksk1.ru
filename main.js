d1e22c85cae187aa883eb35291ac7a4b1a61a849
andrey59 <andrey@kskmedia.ru>
2015-09-17 15:31:37 +0500

    тестирование загрузку погоды в новом меню на ajax

--- main.js
+++ main.js
@@ -458,8 +458,10 @@ jQuery('.dropdown-weather').one('mouseenter',function(){
         timeout: 2000,
         success: function (data) {
             jQuery('.dropdown-weather').find('.dropdown-menu').html(data)
+            console.log('success');
         },
         error: function(msg){
+            console.log('error');
             result = msg.responseText ? msg.responseText : msg.statusText;
             jQuery('.dropdown-weather').find('.dropdown-menu').html("<p>Ошибка: "+result+"<br>Посмотрите на Яндекс.Погода<br> <a class='ya-weather-forecast' href='https://pogoda.yandex.ru/krasnoufimsk/details' target='_blank'><img alt='Погода' src='//info.weather.yandex.net/krasnoufimsk/2_white.ru.png?domain=ru'></a></p>");
         }
