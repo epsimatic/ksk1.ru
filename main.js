1a9e7c976689bd8dc8548a469d78a5299a74f028
andrey59 <andrey@kskmedia.ru>
2015-10-26 17:51:19 +0500

    вставка джингла

--- main.js
+++ main.js
@@ -607,17 +607,18 @@ jQuery(".menu-main-tv_radio").one('mouseenter', function(){
         radio_player.bind(jQuery.jPlayer.event.ended , function(event) {
             jQuery(".play-radio i.fa-play").removeClass('hidden');
             jQuery(".play-radio i.fa-pause").addClass('hidden');
-            radio_player.jPlayer("setMedia",{
+            jQuery("#jquery_jplayer_1").jPlayer({
                ready: function() {
-                   jQuery(this).jPlayer({
+                   jQuery(this).jPlayer("setMedia", {
                    m4a: "http://ksk1.ru/radio-news/radiokruf.m4a"
                    });
                },
                ended : function(){
-                    jQuery(this).jPlayer( {
+                    jQuery(this).jPlayer("setMedia", {
                         m4a: "http://radio.ksk66.ru:8000/aac"
                     });
                },
+               swfPath: "js",
                supplied: "m4a"
             }).jPlayer("play");
         });
