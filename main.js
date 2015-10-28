7f6416791594ce2f6dbe16a6f568c7aa7327c605
andrey59 <andrey@kskmedia.ru>
2015-10-28 14:37:36 +0500

    проверка если плеер играет , то ставим куку

--- main.js
+++ main.js
@@ -716,7 +716,7 @@ function getCookie(key) {
 
 // при закрытии вкладки или браузера
 window.onbeforeunload = function () {
-    if (jQuery(".play-radio #play.hidden")){
+    if (jQuery(".play-radio #play.hidden").length()){
     setCookie("player_state", "live", "30000");
     }
 };
