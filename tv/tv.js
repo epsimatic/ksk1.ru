6f7379f08667ec688645211759fe4a461402c201
andrey59 <andrey@kskmedia.ru>
2015-12-15 14:52:11 +0500

    дата местная6

--- tv/tv.js
+++ tv/tv.js
@@ -14,16 +14,16 @@ function updateClock ( )
     currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
     // Choose either "AM" or "PM" as appropriate
-    var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+    /*var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";*/
 
     // Convert the hours component to 12-hour format if needed
-    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+  /*  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;*/
 
     // Convert an hours component of "0" to "12"
     currentHours = ( currentHours == 0 ) ? 12 : currentHours;
 
     // Compose the string for display
-    var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+    var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds
 
     // Update the time display
     document.getElementById("clock").firstChild.nodeValue = currentTimeString;
