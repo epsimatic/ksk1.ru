4a0bb15eb7f2fbce7c0f5c60d53b0fdebc680489
Oleg <epushiron@gmail.com>
2015-12-16 15:07:02 +0500

    Упростил показ даты / времени

--- tv/tv.js
+++ tv/tv.js
@@ -4,45 +4,31 @@
 function updateClock ( )
 {
     var currentTime = new Date ( );
-    var currentMonth = currentTime.getMonth();
-    var currentDay   = currentTime.getDate();
     var currentHours = currentTime.getHours ( );
     var currentMinutes = currentTime.getMinutes ( );
-    //var currentSeconds = currentTime.getSeconds ( );
-    var nameMonth = {0: "января",
-         1: "февраля",
-         2: "марта",
-         3: "апреля",
-         4: "мая",
-         5: "июня",
-         6: "июля",
-         7: "августа",
-         8: "сентября",
-         9: "октября",
-         10: "ноября",
-         11: "декабря"
-    };
+
+    var nameMonth = [
+         "января",
+         "февраля",
+         "марта",
+         "апреля",
+         "мая",
+         "июня",
+         "июля",
+         "августа",
+         "сентября",
+         "октября",
+         "ноября",
+         "декабря"
+    ];
 
     // Pad the minutes and seconds with leading zeros, if required
     currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-    //currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-
-    // Choose either "AM" or "PM" as appropriate
-    /*var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";*/
-
-    // Convert the hours component to 12-hour format if needed
-  /*  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;*/
-
-    // Convert an hours component of "0" to "12"
-    currentHours = ( currentHours == 0 ) ? '00' : currentHours;
-
-    // Compose the string for display
-  //  var currentTimeString = currentHours + ":" + currentMinutes ;
-  //  var currentDateString = currentDay + " " + nameMonth[currentMonth] ;
+    currentHours   = ( currentHours   < 10 ? "0" : "" ) + currentHours;
 
     // Update the time display
     jQuery("#clock").text(currentHours + ":" + currentMinutes);
-    jQuery("#date").text(currentDay + " " + nameMonth[currentMonth]);
+    jQuery("#date").text(currentTime.getDate() + " " + nameMonth[currentTime.getMonth()]);
 }
 
 // Получить текущий трек
