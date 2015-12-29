e534abada4624a7b7738d35da5000a26d526d41d
Oleg <epushiron@gmall.com>
2015-12-29 17:00:40 +0500

    Рефакторю загрузку скриптов

--- tv/tv.js
+++ tv/tv.js
@@ -68,11 +68,54 @@ jQuery(document).ready(function() {
     UpdateBlockUpdateTimer("#clock, #date", updateClock, timers['clock']);
 
     // Запускает бегущую строку  http://jonmifsud.com/open-source/jquery/jquery-webticker/
-    jQuery('#webticker').webTicker({
-        speed: 150,
-        rssurl: 'http://brief.kskmedia.ru/feed/',
-        rssfrequency: timers['ticker'] / 60,
-        hoverpause: false
+    LoadJS('/tv/news-ticker.js', function(){
+        jQuery('#webticker').webTicker({
+            speed: 150,
+            rssurl: 'http://brief.kskmedia.ru/feed/',
+            rssfrequency: timers['ticker'] / 60,
+            hoverpause: false
+        });
     });
-
 });
+
+window.libsAvail   = [];
+window.libsLoading = [];
+function LoadRes(src, type, callback) {
+    var resName = src.split("/").reverse()[0];
+    if (libsAvail.indexOf(resName) != -1) {
+        console.log("Available already: «" + resName + "» " + src +
+            ((typeof (callback) == "function") ? ", running callback" : ""));
+        if (typeof (callback) == "function") callback();
+    } else if (libsLoading.indexOf(resName) != -1) {
+        console.log("Still loading, retry: «" + resName + "» " + src);
+        window.setTimeout( function() { LoadRes(src, type, callback) }, 100);
+    } else {
+        window.libsLoading.push(resName);
+        console.log("Loading «" + resName + "» " + src);
+        var e = document.createElement(type);
+        if (type == 'script') {
+            e.type = "text/javascript";
+            e.src = src;
+            e.async = true;
+        } else {
+            e.type = "text/css";
+            e.href = src;
+            e.rel = "stylesheet";
+        }
+        e.onerror = function () {
+            console.error("Error loading " + src);
+            window.libsLoading.splice(window.libsLoading.indexOf(resName),1);
+        };
+        e.onload = function () {
+            resName = src.split("/").reverse()[0];
+            console.log("Loaded «" + resName + "» " + src +
+                ((typeof (callback) == "function") ? ", running callback" : ""));
+            window.libsAvail.push(resName);
+            window.libsLoading.splice(window.libsLoading.indexOf(resName),1);
+            if (typeof (callback) == "function") callback();
+        };
+        document.getElementsByTagName("head")[0].appendChild(e);
+    }
+}
+function LoadJS (src, onload) {LoadRes(src, 'script', onload)}
+function LoadCSS(src, onload) {LoadRes(src, 'link', onload)}
