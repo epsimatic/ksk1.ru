3f3781d7c8e2264667ffadf9cd7fd776aa6d9485
andrey59 <andrey@kskmedia.ru>
2015-12-31 10:57:22 +0500

    увеличил период перезагрузки main на экране

--- tv/tv.js
+++ tv/tv.js
@@ -3,7 +3,7 @@
  */
 
 const default_timeouts = { // Периоды обновления панелей в секундах
-    ".board-main": 30000,
+    ".board-main": 300000000,
     ".board-yummie": 42,
     "#clock, #date": 10,
     ".track-data-text": 15,
