9f6315e295ea4b46dfca297b9c9dec224700c64b
andrey59 <andrey@kskmedia.ru>
2016-03-04 16:18:39 +0500

    всё вернул

--- tv/tv.js
+++ tv/tv.js
@@ -194,8 +194,7 @@ LoadJS("http://ksk1.ru/js/jquery-1.js", function () {
             play: function (/*event*/) {
                 //jQuery(".play-radio i.fa-play").addClass('hidden');
                 //jQuery(".play-radio i.fa-pause").removeClass('hidden');
-                var iframe = document.getElementsByTagName('iframe')[0];
-                if(iframe) radio_player.jPlayer("pause");
+
             //    if (typeof(video_player) != 'undefined') {
             //        video_player.stopVideo();
              //   }
