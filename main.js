f08ad6dc27dd227de8fb7c679bbbda09f5616de2
andrey59 <andrey@kskmedia.ru>
2016-08-10 13:42:14 +0500

    openstat поменял image 2

--- main.js
+++ main.js
@@ -48,8 +48,8 @@
 //  Openstat
 // FIXME: загружать, только если есть подходящий элемент.
 var openstat = {
-    counter: 2173092, image: 5081, color: "828282", next: openstat,
-    part: jQuery('body').prop('class').split(' ')[0]
+    counter: 2173092, image: 5081, color: "828282", next: openstat
+    //, part: jQuery('body').prop('class').split(' ')[0]
 };
 LoadJS('//openstat.net/cnt.js');
 
