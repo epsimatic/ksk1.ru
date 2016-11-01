33990227f495c0fe3bafe3b97eb6a01f0b4ab753
andrey59 <andrey@kskmedia.ru>
2016-11-01 14:44:36 +0500

    ссылка на погоду в мобильной версии weather-block

--- weather/get_weather.php
+++ weather/get_weather.php
@@ -93,11 +93,11 @@ else {
         11 => "ноября",
         12 => "декабря",
     );
-    $conditions = '<a href="https://pogoda.yandex.ru/krasnoufimsk/details"><div class="weather-text">'
+    $conditions = '<div class="weather-text">'
                 . '<div class="weather-date">' .date("j", $parsed_conditions->{'observation_epoch'})." ".$month[date("n", $parsed_conditions->{'observation_epoch'})] . '</div>'
                 . '<div class="weather-label">' . $description . '</div>'
                 . '</div>'
-                . '<div class="weather-block" title="По данным на '
+                . '<a href="https://pogoda.yandex.ru/krasnoufimsk/details"><div class="weather-block" title="По данным на '
                 . $week[date("l", $parsed_conditions->{'observation_epoch'})] . " в "
                 . date("G.i", $parsed_conditions->{'observation_epoch'}) . ':' . PHP_EOL
                 . $temp . PHP_EOL
