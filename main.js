b0b2f7e70a36c203fd0d07c98a7751068bdcffc5
andrey59 <andrey@kskmedia.ru>
2017-03-27 13:36:01 +0500

    вывел hostname в консоль

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
  * Основные функции общего шаблона
  */
 var hostname=document.location.hostname;
+console.log("хост "+hostname);
 // Поисковая строка
 (function () {
     const meta_generator = jQuery("meta[name='generator']").attr("content");
