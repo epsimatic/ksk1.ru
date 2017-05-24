387fea91f4d98df0a4dac00793554ad4d52b5a1f
andrey59 <andrey@kskmedia.ru>
2017-05-24 15:57:02 +0500

    Openstat поменял код3

--- main.js
+++ main.js
@@ -52,12 +52,12 @@ var hostname=document.location.hostname;
     part: jQuery('body').prop('class').split(' ')[0]
 };
 LoadJS('//openstat.net/cnt.js');*/
-var openstat = { counter: 2173092, image: 5088, color: "828282", next: openstat };
+/*var openstat = { counter: 2173092, image: 5088, color: "828282", next: openstat };
 (function(d, t, p) {
     var j = d.createElement(t); j.async = true; j.type = "text/javascript";
     j.src = ("https:" == p ? "https:" : "http:") + "//openstat.net/cnt.js";
     var s = d.getElementsByTagName(t)[0]; s.parentNode.insertBefore(j, s);
-})(document, "script", document.location.protocol);
+})(document, "script", document.location.protocol);*/
 
 // Yandex.Metrika counter
 /*
