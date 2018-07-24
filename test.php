7b32e9fcd9707ae703111ad7615dd72598b0f24d
andrey59 <andrey@kskmedia.ru>
2018-07-24 21:51:39 +0500

    запрос курл к яндексу

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+$ch = curl_init( "https://api.weather.yandex.ru/v1/informers?lat=56.618007&lon=57.779208");
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE );
+curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, false );
+curl_setopt( $ch, CURLOPT_TIMEOUT, 3 );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, 'X-Yandex-API-Key: 90b3db9b-dcf5-4b5e-b8e0-8c14d83620a9');
+
+$response = json_decode(curl_exec( $ch ));
+$error_code = curl_errno($ch);
+curl_close($ch);
+var_dump($response);
