718a28e9af56528ad27698000380839c0f009494
andrey59 <andrey@kskmedia.ru>
2018-07-24 22:42:08 +0500

    запрос курл к яндексу2

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE );
 curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
 curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, false );
 curl_setopt( $ch, CURLOPT_TIMEOUT, 3 );
-curl_setopt( $ch, CURLOPT_HTTPHEADER, 'X-Yandex-API-Key: 90b3db9b-dcf5-4b5e-b8e0-8c14d83620a9');
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('X-Yandex-API-Key: 90b3db9b-dcf5-4b5e-b8e0-8c14d83620a9'));
 
 $response = json_decode(curl_exec( $ch ));
 $error_code = curl_errno($ch);
