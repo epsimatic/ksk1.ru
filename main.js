c6780c218c40d0c75c88e26e9691c8305a2c0931
andrey59 <andrey@kskmedia.ru>
2018-12-03 19:01:17 +0500

    удалил openstat

--- main.js
+++ main.js
@@ -62,12 +62,12 @@ LoadJS('//openstat.net/cnt.js');*/
 <!--Openstat-->
 
 
-var openstat = { counter: 2173092, image: 5081, color: "828282", next: openstat };
+/*var openstat = { counter: 2173092, image: 5081, color: "828282", next: openstat };
 (function(d, t, p) {
     var j = d.createElement(t); j.async = true; j.type = "text/javascript";
     j.src = ("https:" == p ? "https:" : "http:") + "//openstat.net/cnt.js";
     var s = d.getElementsByTagName(t)[0]; s.parentNode.insertBefore(j, s);
-})(document, "script", document.location.protocol);
+})(document, "script", document.location.protocol);*/
 
     <!--/Openstat-->
 
